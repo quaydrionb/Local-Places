6ac8b804f92d33965bdc9831b3ea7cf9358bcadd
Quaydrion Butler <99278263+quaydrionb@users.noreply.github.com>
2024-01-12 21:31:16 -0500

    Updated card style and font

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,19 +3,7 @@ import styles from "./App.module.css";
 
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
-
-const business = {
-  imageSrc:
-    "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
-  name: "Quad Pizzeria",
-  address: "123 Pizza Square",
-  city: "Cityville",
-  state: "FL",
-  zipCode: "12345",
-  category: "Italian",
-  rating: 4.5,
-  reviewCount: 90,
-};
+import business from "./BussinesData";
 
 const businesses = [business, business, business, business, business, business];
 
---
+++ src/components/App/BussinesData.js
@@ -0,0 +1,14 @@
+const business = {
+  imageSrc:
+    "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+  name: "Quad Pizzeria",
+  address: "123 Pizza Square",
+  city: "Cityville",
+  state: "FL",
+  zipCode: "12345",
+  category: "Italian",
+  rating: 4.5,
+  reviewCount: 90,
+};
+
+export default business;
