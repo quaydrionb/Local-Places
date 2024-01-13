4021986fae004f76c14261e49c92a0ac7f1d306d
Quaydrion Butler <99278263+quaydrionb@users.noreply.github.com>
2024-01-12 19:43:01 -0500

    Updated colors

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,11 +7,11 @@ import SearchBar from "../SearchBar/SearchBar";
 const business = {
   imageSrc:
     "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
-  name: "MarginOtto Pizzeria",
-  address: "1010 Paddington Way",
-  city: "Bordertown",
-  state: "NY",
-  zipCode: "10101",
+  name: "Quad Pizzeria",
+  address: "123 Pizza Square",
+  city: "Cityville",
+  state: "FL",
+  zipCode: "12345",
   category: "Italian",
   rating: 4.5,
   reviewCount: 90,
@@ -22,7 +22,7 @@ const businesses = [business, business, business, business, business, business];
 const App = () => {
   return (
     <div className={styles.App}>
-      <h1>ravenous</h1>
+      <h1>Local Places</h1>
       <SearchBar />
       <BusinessList businesses={businesses} />
     </div>
