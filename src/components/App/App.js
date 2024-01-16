29a1f4d0bc24ff6fab9eac69c8b09f179dae12ff
Quaydrion Butler <99278263+quaydrionb@users.noreply.github.com>
2024-01-15 21:48:42 -0500

    updated searchbar.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,10 +8,14 @@ import business from "./BussinesData";
 const businesses = [business, business, business, business, business, business];
 
 const App = () => {
+  const searchYelp = (term, location, sortBy) => {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  };
+
   return (
     <div className={styles.App}>
       <h1>Local Places</h1>
-      <SearchBar />
+      <SearchBar searchYelp={searchYelp} />
       <BusinessList businesses={businesses} />
     </div>
   );
